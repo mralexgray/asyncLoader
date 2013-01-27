7f86ad454582518a5a9b40fa8355969095cf0f56
Adrian Soluch <adrian@soluch.at>
2013-01-27 22:51:39 +0100

    minor js optimizations

--- asyncLoader.js
+++ asyncLoader.js
@@ -56,9 +56,6 @@
         link.rel = 'stylesheet';
         link.href = url;
         document.getElementsByTagName('head')[0].appendChild( link );
-    },
-    fileReady = function() {
-        jsFilesReady++;
     }
     // simple foreach implementation
     Array.prototype.foreach = function( callback ) {
