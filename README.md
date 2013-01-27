f8287f8b9976b74cbbd58217106bd4145c50a324
Adrian Soluch <adrian@soluch.at>
2013-01-27 20:39:03 +0100

    init

---
+++ README.md
@@ -0,0 +1,40 @@
+# asyncLoader
+
+### asyncLoader - simple asynchronous script and stylesheet loading with ( optional ) callback.
+
+
+##### live examples:
+[async.soluch.at](http://async.soluch.at/)
+The live example page heavy utilizes Twitter Bootstrap & Font Awesome 3.0
+
+##### usage:
+
+The only script you need to load ( in head ) is asyncLoader.js :
+```html
+<script src="asyncLoader.js"></script>
+```
+
+Then load everything else you need & initialise via callback ( which is, by the way, optional ).
+```javascript
+asyncLoader(
+    [
+        'css/bootstrap.min.css',
+        'css/bootstrap-responsive.min.css',
+        'css/font-awesome.css',
+        'http://yandex.st/highlightjs/7.3/styles/pojoaque.min.css',
+        '//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js',
+        '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.0/jquery-ui.min.js',
+        'http://yandex.st/highlightjs/7.3/highlight.min.js'
+    ],
+    {
+        'callback':function() {
+            $('body').fadeIn( 800 );
+            $('#test').delay(800).fadeIn();
+            // Syntax Highlighting
+            $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
+        }
+    }
+);
+```
+##### Copyright and License
+asyncLoader is licensed under the terms of the MIT License, see the included MIT-LICENSE file.
---
+++ asyncLoader.js
@@ -0,0 +1,69 @@
+/* ============================================================
+ * asyncLoader.js v1.0.0
+ * simple asynchronous script and stylesheet loading with callback
+ * http://async.soluch.at/
+ * ============================================================
+ * Copyright 2013, Adrian Soluch - http://soluch.at/
+ * Released under the MIT License 
+ * ============================================================ */
+
+( function ( window, doc ) {
+
+    asyncLoader = function( urls, options ) {
+
+        urls.foreach( function( i, file ) {
+            loadFile( file, getExtension( file ), options );
+        });
+
+        // checking for a callback function
+        if( typeof options.callback == 'function') {
+            // calling the callback
+            var readyStateCheckInterval = setInterval( function() {
+                if( doc.readyState === 'complete' ) {
+                    clearInterval( readyStateCheckInterval );
+                        options.callback();
+                }
+            }, 10 );
+        }
+    }
+
+    var getExtension = function( file ) {
+        var extension = file.split('.');
+        return extension[extension.length - 1];
+    }, 
+    loadFile = function( file, ext, options ) {
+        switch( ext ) {
+            case 'js' :
+                loadJs( file, options );
+            break;
+            case 'css' :
+                loadCss( file );
+            break;
+            default :
+            break;
+        }
+    },
+    loadJs = function( url, options ) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.src = url;
+        document.getElementsByTagName('head')[0].appendChild( script );
+    },
+    loadCss = function( url ) {
+        var link = document.createElement('link');
+        link.type = 'text/css';
+        link.rel = 'stylesheet';
+        link.href = url;
+        document.getElementsByTagName('head')[0].appendChild( link );
+    },
+    fileReady = function() {
+        jsFilesReady++;
+    }
+    // simple foreach implementation
+    Array.prototype.foreach = function( callback ) {
+        for( var i=0; i<this .length; i++ ) {
+            callback( i, this[ i ] );
+        }
+    }
+})( this, document );
